9721a4c001251d986cec8f61620ab0c3008541af
Gökçen Eraslan <gokcen.eraslan@gmail.com>
2014-12-24 00:43:43 +0100

    pytgl: Add more C methods to TglState class

--- pytgl/tgl.py
+++ pytgl/tgl.py
@@ -4,14 +4,23 @@ from .callbacks import *
 
 class TglState(object):
     def __init__(self):
-        self._state = ffi.new('tgl_state *')
-        self._update_cb = ffi.new('tgl_update_callback *')
+        self._state = ffi.new('struct tgl_state *')
+        self._update_cb = ffi.new('struct tgl_update_callback *')
 
     def set_callback(self, cb):
         tgl.tgl_set_callback(self._state, cb)
 
     def set_net_methods(self, m):
-        tgl.tgl_set_new_methods(self._state, m)
+        tgl.tgl_set_net_methods(self._state, m)
+
+    def set_timer_methods(self, m):
+        tgl.tgl_set_timer_methods(self._state, m)
+
+    def set_download_directory(self, dd):
+        tgl.tgl_set_download_directory(self._state, dd)
+
+    def register_app_id(self, id_, hash_):
+        tgl.tgl_register_app_id(self._state, id_, hash_)
 
     def init(self):
         tgl.tgl_init(self._state)
