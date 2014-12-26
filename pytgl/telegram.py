17ec1297af38a8a127a55cd80f1401e89c1e64a5
Gökçen Eraslan <gokcen.eraslan@gmail.com>
2014-12-26 20:59:21 +0100

    pytgl: Move everthing required by tgl_init to class __init__ method and make Python API more Pythonic.

--- pytgl/telegram.py
+++ pytgl/telegram.py
@@ -2,16 +2,39 @@ from . import ffi, tgl
 
 from .callbacks import *
 
-class TglState(object):
-    def __init__(self):
+class Telegram(object):
+    def __init__(self,
+                 rsa_key,
+                 download_dir,
+                 update_callbacks,
+                 app_id,
+                 app_hash):
+
         self._state = ffi.new('struct tgl_state *')
-        self._download_dir = None
+        self._download_dir = download_dir
+
+        #files
         self._auth_file = None
         self._secret_chat_file = None
         self._state_file = None
-        self._appid = -1
-        self._apphash = None
-        self._rsa_keypath = None
+
+        self._app_id = app_id
+        self._app_hash = app_hash
+        self._rsa_keypath = rsa_key
+
+        self.set_callback(update_callbacks)
+
+        self.set_net_methods(ffi.addressof(tgl.tgl_conn_methods))
+        self.set_timer_methods(ffi.addressof(tgl.tgl_libevent_timers))
+        self.set_serialize_methods(ffi.addressof(tgl.tgl_file_methods))
+
+        self.set_rsa_key(self._rsa_keypath)
+
+        self.set_download_directory(self._download_dir)
+
+        self.register_app_id(self._app_id, self._app_hash)
+
+        tgl.tgl_init(self._state)
 
     def set_callback(self, cb):
         tgl.tgl_set_callback(self._state, cb)
@@ -21,6 +44,9 @@ class TglState(object):
 
     def set_timer_methods(self, m):
         self._state.timer_methods = m
+
+    def set_serialize_methods(self, m):
+        self._state.serialize_methods = m
 
     def set_download_directory(self, dd):
         self._download_dir = dd
@@ -49,14 +75,10 @@ class TglState(object):
         tgl.tgl_register_app_id(self._state, self._appid, self._apphash)
 
     def load_auth(self):
-        tgl.tgl_file_methods.load_auth(self._state)
+        self._state.serialize_methods.load_auth(self._state)
 
     def load_state(self):
-        tgl.tgl_file_methods.load_state(self._state)
+        self._state.serialize_methods.load_state(self._state)
 
     def load_secret_chats(self):
-        tgl.tgl_file_methods.load_secret_chats(self._state)
-
-    def init(self):
-        tgl.tgl_init(self._state)
-
+        self._state.serialize_methods.load_secret_chats(self._state)
--- scripts/pytgl-example.py
+++ scripts/pytgl-example.py
@@ -3,37 +3,25 @@
 import sys
 import os
 
-import pytgl
-
-from pytgl.tgl import TglState
+from pytgl.telegram import Telegram
 from pytgl.callbacks import *
 
-s = TglState()
-
-s.set_rsa_key(sys.argv[1])
+if len(sys.argv) != 2:
+    print "Usage: %s telegram_rsa_key_path" % sys.argv[0]
+    sys.exit()
 
 upd_cb = generate_tgl_update()
-net_cb = generate_tgl_net()
-timer_cb = generate_tgl_timer()
-mtproto_cb = generate_tgl_mtproto()
 
-s.set_callback(upd_cb)
-s.set_net_methods(net_cb)
-s.set_timer_methods(timer_cb)
+tg = Telegram(rsa_key = sys.argv[1],
+              download_dir = '/tmp',
+              update_callbacks = upd_cb,
+              app_id = 2899,
+              app_hash = "36722c72256a24c1225de00eb6a1ca74")
 
-s.set_download_directory('/tmp')
+tg.set_auth_file(os.path.expanduser('~/.telegram-cli/auth'))
+tg.set_state_file(os.path.expanduser('~/.telegram-cli/state'))
+tg.set_secret_chat_file(os.path.expanduser('~/.telegram-cli/secret'))
 
-apphash = "36722c72256a24c1225de00eb6a1ca74"
-s.register_app_id(2899, apphash)
-s.init()
-
-s.set_auth_file(os.path.expanduser('~/.telegram-cli/auth'))
-s.set_state_file(os.path.expanduser('~/.telegram-cli/state'))
-s.set_secret_chat_file(os.path.expanduser('~/.telegram-cli/secret'))
-
-s.load_auth()
-s.load_secret_chats()
-s.load_state()
-
-
-#read auth state secret
+tg.load_auth()
+tg.load_secret_chats()
+tg.load_state()
