0b0fac245ef531f6e3a83ed0d2145ff3c91dec9d
Gökçen Eraslan <gokcen.eraslan@gmail.com>
2015-01-05 13:03:34 +0100

    Switch to Python3 and move all boilerplate code to Telegram class methods.

--- pytgl/telegram.py
+++ pytgl/telegram.py
@@ -2,21 +2,29 @@ from . import ffi, tgl
 
 from .callbacks import *
 
+import os
+import tempfile
+
 class Telegram(object):
     def __init__(self,
                  rsa_key,
-                 download_dir,
                  update_callbacks,
-                 app_id,
-                 app_hash):
+                 app_id = 2899,
+                 app_hash = "36722c72256a24c1225de00eb6a1ca74",
+                 download_dir = None,
+                 config_dir = os.path.expanduser('~/.telegram-cli')):
 
         self._state = ffi.new('struct tgl_state *')
         self._download_dir = download_dir
+        self._config_dir = config_dir
 
-        #files
-        self._auth_file = None
-        self._secret_chat_file = None
-        self._state_file = None
+        #dirs and files
+        if not os.path.exists(self._config_dir):
+            os.makedirs(self._config_dir)
+
+        self._auth_file = os.path.join(self._config_dir, 'auth')
+        self._state_file = os.path.join(self._config_dir, 'state')
+        self._secret_chat_file = os.path.join(self._config_dir, 'secret')
 
         self._app_id = app_id
         self._app_hash = app_hash
@@ -31,11 +39,22 @@ class Telegram(object):
 
         self.set_rsa_key(self._rsa_keypath)
 
+        if self._download_dir is None:
+            self._download_dir = tempfile.mkdtemp(prefix = 'telegram')
+
         self.set_download_directory(self._download_dir)
 
         self.register_app_id(self._app_id, self._app_hash)
 
         tgl.tgl_init(self._state)
+
+        self.set_auth_file(self._auth_file)
+        self.set_state_file(self._state_file)
+        self.set_secret_chat_file(self._secret_chat_file)
+
+        self.load_auth()
+        self.load_state()
+        self.load_secret_chats()
 
     def set_callback(self, cb):
         tgl.tgl_set_callback(self._state, cb)
@@ -51,29 +70,32 @@ class Telegram(object):
 
     def set_download_directory(self, dd):
         self._download_dir = dd
-        tgl.tgl_set_download_directory(self._state, self._download_dir)
+        tgl.tgl_set_download_directory(self._state,
+                self._download_dir.encode())
 
     def set_auth_file(self, path):
         self._auth_file = path
-        tgl.tgl_set_auth_file_path(self._state, self._auth_file)
+        tgl.tgl_set_auth_file_path(self._state, self._auth_file.encode())
 
     def set_secret_chat_file(self, path):
         self._secret_chat_file = path
-        tgl.tgl_set_secret_chat_file_path(self._state, self._secret_chat_file)
+        tgl.tgl_set_secret_chat_file_path(self._state,
+                self._secret_chat_file.encode())
 
     def set_state_file(self, path):
         self._state_file = path
-        tgl.tgl_set_state_file_path(self._state, self._state_file)
+        tgl.tgl_set_state_file_path(self._state, self._state_file.encode())
 
     def set_rsa_key(self, keypath):
         self._rsa_keypath = keypath
-        tgl.tgl_set_rsa_key(self._state, self._rsa_keypath)
+        tgl.tgl_set_rsa_key(self._state, self._rsa_keypath.encode())
 
     def register_app_id(self, id_, hash_):
         self._appid = id_
         self._apphash = hash_
 
-        tgl.tgl_register_app_id(self._state, self._appid, self._apphash)
+        tgl.tgl_register_app_id(self._state, self._appid,
+                self._apphash.encode())
 
     def load_auth(self):
         self._state.serialize_methods.load_auth(self._state)
@@ -107,3 +129,93 @@ class Telegram(object):
         return all([tgl.tgl_authorized_dc(s, s.DC_list[i])
                    for i in range(1, max_dc+1)]) #WTF!!!
 
+    def check_authorization(self):
+
+        for i in range(1, self._state.max_dc_num+1):
+            if self._state.DC_list[i] and not tgl.tgl_authorized_dc(self._state, self._state.DC_list[i]):
+                raise Exception("DC[%d] is not authorized." % (i-1))
+
+    def wait_until_authorization(self):
+        self.loop(0, lambda: int(self.all_authorized()))
+
+
+    def reset_authorization(self):
+        tgl.bl_do_reset_authorization(self._state)
+
+    def signed_in(self):
+        tgl.tgl_signed_dc (self._state, self._state.DC_working)
+
+    def sign_in(self):
+        if not self.signed_in():
+            username = input("Telephone number (with '+' sign): ").encode()
+
+            registered = False
+            hash_ = None
+
+            @ffi.callback("void (*)(struct tgl_state *, void *, int , int , const char *)")
+            def sign_in_cb(tls, extra, success, registeredarg, mhasharg):
+
+                nonlocal registered
+                nonlocal hash_
+
+                if success:
+                    registered = registeredarg
+                    hash_ = mhasharg
+
+            tgl.tgl_do_send_code (self._state, username, sign_in_cb, ffi.NULL);
+            self.loop(0, lambda: hash_ is not None)
+
+            if registered:
+                print('Code from SMS (if you did not receive an SMS and want' \
+                      ' to be called, type "call"): ', end=' ')
+
+                while True:
+                    code = input()
+                    if code.strip() == 'call':
+                        tgl.tgl_do_phone_call (self._state, username, hash_, ffi.NULL, ffi.NULL);
+                        print("Code: ", end=' ')
+                        continue
+
+                    global _signed_in
+                    _signed_in = False
+                    @ffi.callback('void (*)(struct tgl_state *TLSR, void *extra, int' \
+                                  ' success, struct tgl_user *U)')
+                    def sign_in_result_cb(tls, extra, success, user):
+                        global _signed_in
+                        if success:
+                            _signed_in = True
+
+                    if tgl.tgl_do_send_code_result(self._state, username,
+                            hash_, code.encode(), sign_in_result_cb, ffi.NULL) >= 0:
+                        break
+                    print("Invalid code. Try again: ", end=' ')
+
+            else:
+                print("Registration is not implemented yet...")
+                sys.exit(0)
+
+            self.loop(0, lambda: int(signed_in))
+
+    def check_sign_in(self):
+        for i in range(1, self._state.max_dc_num+1):
+            if self._state.DC_list[i] and not tgl.tgl_signed_dc(self._state, self._state.DC_list[i]):
+                tgl.tgl_do_export_auth(self._state, i-1, ffi.NULL, self._state.DC_list[i])
+                self.loop(0, lambda: tgl.tgl_signed_dc(self._state, self._state.DC_list[i]))
+                assert tgl.tgl_signed_dc(self._state, self._state.DC_list[i])
+
+    def send_all_unsent(self):
+        tgl.tglm_send_all_unsent(self._state)
+
+    def get_difference(self):
+        diff_success = 0
+        @ffi.callback("void (*)(struct tgl_state *, void *, int)")
+        def get_diff_cb(tls, extra, success):
+            global diff_success
+            if success:
+                diff_success = 1
+
+
+        tgl.tgl_do_get_difference(self._state, 0, get_diff_cb, ffi.NULL)
+
+        self.loop(0, lambda: diff_success)
+        self._state.started = 1
--- scripts/pytgl-example.py
+++ scripts/pytgl-example.py
@@ -1,14 +1,13 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
-import os
 
-from pytgl.telegram import Telegram, tgl
+from pytgl.telegram import Telegram
 from pytgl import ffi
 
 
 def print_message(tls, msg):
-    print "new msg: ", ffi.string(msg.message)
+    print("new msg: ", ffi.string(msg.message))
 
 @ffi.callback("void(struct tgl_state *, struct tgl_message *)")
 def _tgl_upd_new_msg_cb(tls, msg):
@@ -17,67 +16,67 @@ def _tgl_upd_new_msg_cb(tls, msg):
 
 @ffi.callback("void(struct tgl_state *, int, struct tgl_message *[])")
 def _tgl_upd_marked_read_cb(tls, num, msg_list):
-    print "_tgl_upd_marked_read_cb"
+    print("_tgl_upd_marked_read_cb")
     pass
 
 
 @ffi.callback("void(struct tgl_state *, struct tgl_user*, enum tgl_typing_status)")
 def _tgl_upd_type_notification_cb(tls, user, status):
-    print "_tgl_upd_type_notification_cb"
+    print("_tgl_upd_type_notification_cb")
     pass
 
 
 @ffi.callback("void(struct tgl_state *, struct tgl_user *, struct tgl_chat*, enum tgl_typing_status)")
 def _tgl_upd_type_in_chat_notification_cb(tls, user, chat, status):
-    print "_tgl_upd_type_in_chat_notification_cb"
+    print("_tgl_upd_type_in_chat_notification_cb")
     pass
 
 
 @ffi.callback("void(struct tgl_state *, struct tgl_secret_chat *)")
 def _tgl_upd_type_in_secret_chat_notification_cb(tls, chat):
-    print "_tgl_upd_type_in_secret_chat_notification_cb"
+    print("_tgl_upd_type_in_secret_chat_notification_cb")
     pass
 
 
 @ffi.callback("void(struct tgl_state *, struct tgl_user *)")
 def _tgl_upd_status_notification_cb(tls, user):
-    print "_tgl_upd_status_notification_cb"
+    print("_tgl_upd_status_notification_cb")
     pass
 
 
 @ffi.callback("void(struct tgl_state *, struct tgl_user *)")
 def _tgl_upd_user_registered_cb(tls, user):
-    print "_tgl_upd_user_registered_cb"
+    print("_tgl_upd_user_registered_cb")
     pass
 
 
 @ffi.callback("void(struct tgl_state *, struct tgl_user *)")
 def _tgl_upd_user_activated_cb(tls, user):
-    print "_tgl_upd_user_activated_cb"
+    print("_tgl_upd_user_activated_cb")
     pass
 
 
 @ffi.callback("void(struct tgl_state *, const char *, const char*)")
 def _tgl_upd_new_authorization_cb(tls, device, location):
-    print "_tgl_upd_new_authorization_cb"
+    print("_tgl_upd_new_authorization_cb")
     pass
 
 
 @ffi.callback("void(struct tgl_state *, struct tgl_chat *, unsigned)")
 def _tgl_upd_chat_update_cb(tls, chat, flags):
-    print "_tgl_upd_chat_update_cb"
+    print("_tgl_upd_chat_update_cb")
     pass
 
 
 @ffi.callback("void(struct tgl_state *, struct tgl_user *, unsigned)")
 def _tgl_upd_user_update_cb(tls, chat, flags):
-    print "_tgl_upd_user_update_cb"
+    print("_tgl_upd_user_update_cb")
     pass
 
 
 @ffi.callback("void(struct tgl_state *, struct tgl_secret_chat *, unsigned)")
 def _tgl_upd_secret_chat_update_cb(tls, chat, flags):
-    print "_tgl_upd_secret_chat_update_cb"
+    print("_tgl_upd_secret_chat_update_cb")
     pass
 
 
@@ -88,19 +87,19 @@ def _tgl_upd_msg_receive_cb(tls, msg):
 
 @ffi.callback("void(struct tgl_state *, int)")
 def _tgl_upd_our_id_cb(tls, our_id):
-    print "_tgl_upd_our_id_cb"
+    print("_tgl_upd_our_id_cb")
     pass
 
 
 @ffi.callback("void(struct tgl_state *TLS, char *, char *)")
 def _tgl_upd_notification_cb(tls, typ, msg):
-    print "_tgl_upd_notification_cb"
+    print("_tgl_upd_notification_cb")
     pass
 
 
 @ffi.callback("void(struct tgl_state *, struct tgl_user *)")
 def _tgl_upd_user_status_update_cb(tls, user):
-    print "_tgl_upd_user_status_update_cb"
+    print("_tgl_upd_user_status_update_cb")
     pass
 
 
@@ -136,107 +135,28 @@ def generate_tgl_update():
 
 
 if len(sys.argv) != 2:
-    print "Usage: %s telegram_rsa_key_path" % sys.argv[0]
+    print("Usage: %s telegram_rsa_key_path" % sys.argv[0])
     sys.exit()
 
 upd_cb = generate_tgl_update()
 
 tg = Telegram(rsa_key = sys.argv[1],
-              download_dir = '/tmp',
-              update_callbacks = upd_cb,
-              app_id = 2899,
-              app_hash = "36722c72256a24c1225de00eb6a1ca74")
+              update_callbacks = upd_cb)
 
-tg.set_auth_file(os.path.expanduser('~/.telegram-cli/auth'))
-tg.set_state_file(os.path.expanduser('~/.telegram-cli/state'))
-tg.set_secret_chat_file(os.path.expanduser('~/.telegram-cli/secret'))
+#tg.reset_authorization()
+tg.wait_until_authorization()
+tg.check_authorization()
+print("All DCs are authorized.")
 
-tg.load_auth()
-tg.load_state()
-tg.load_secret_chats()
-
-#tgl.bl_do_reset_authorization(tg._state)
-
-tg.loop(0, lambda: int(tg.all_authorized()))
-print "All DCs are authorized."
-
-for i in range(1, tg._state.max_dc_num+1):
-    if tg._state.DC_list[i] and not tgl.tgl_authorized_dc(tg._state, tg._state.DC_list[i]):
-        assert False
-
-if not tgl.tgl_signed_dc (tg._state, tg._state.DC_working):
-    username = raw_input("Telephone number (with '+' sign): ")
-
-    registered = False
-    hash_ = None
-
-    @ffi.callback("void (*)(struct tgl_state *, void *, int , int , const char *)")
-    def sign_in_cb(tls, extra, success, registeredarg, mhasharg):
-        global registered
-        global hash_
-
-        if success:
-            registered = registeredarg
-            hash_ = mhasharg
-
-    tgl.tgl_do_send_code (tg._state, username, sign_in_cb, ffi.NULL);
-    tg.loop(0, lambda: hash_ is not None)
-
-    if registered:
-        print 'Code from SMS (if you did not receive an SMS and want' \
-              ' to be called, type "call"): ',
-
-        while True:
-            code = raw_input()
-            if code.strip() == 'call':
-                tgl.tgl_do_phone_call (tg._state, username, hash_, ffi.NULL, ffi.NULL);
-                print "Code: ",
-                continue
-
-            signed_in = False
-            @ffi.callback('void (*)(struct tgl_state *TLSR, void *extra, int' \
-                          ' success, struct tgl_user *U)')
-            def sign_in_result_cb(tls, extra, success, user):
-                global signed_in
-                if success:
-                    signed_in = True
-
-            if tgl.tgl_do_send_code_result(tg._state, username, hash_, code,
-                    sign_in_result_cb, ffi.NULL) >= 0:
-                break
-            print "Invalid code. Try again: ",
-
-    else:
-        print "Registration is not implemented yet..."
-        sys.exit(0)
-
-    tg.loop(0, lambda: int(signed_in))
-
-for i in range(1, tg._state.max_dc_num+1):
-    if tg._state.DC_list[i] and not tgl.tgl_signed_dc(tg._state, tg._state.DC_list[i]):
-        tgl.tgl_do_export_auth(tg._state, i-1, ffi.NULL, tg._state.DC_list[i])
-        tg.loop(0, lambda: tgl.tgl_signed_dc(tg._state, tg._state.DC_list[i]))
-        assert tgl.tgl_signed_dc(tg._state, tg._state.DC_list[i])
+tg.sign_in()
+tg.check_sign_in()
+print("Signed in.")
 
 tg.store_auth()
 
-tgl.tglm_send_all_unsent(tg._state)
-print "All unsent msgs are sent."
+tg.send_all_unsent()
+print("All unsent msgs are sent.")
 
-diff_success = 0
-@ffi.callback("void (*)(struct tgl_state *, void *, int)")
-def get_diff_cb(tls, extra, success):
-    global diff_success
-    if success:
-        diff_success = 1
-
-
-tgl.tgl_do_get_difference(tg._state, 0, get_diff_cb, ffi.NULL)
-
-tg.loop(0, lambda: diff_success)
-print "Got difference."
-
-tg._state.started = 1
-
-print "Entering main loop..."
+tg.get_difference()
+print("Entering main loop...")
 tg.loop()
