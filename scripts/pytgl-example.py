6069290ccdb4ccb365fe53ac1437a53fabf553bc
H. İbrahim Güngör <igungor@gmail.com>
2015-01-09 02:07:13 +0200

    scripts: Reorganize pytgl-example.py

--- scripts/pytgl-example.py
+++ scripts/pytgl-example.py
@@ -2,8 +2,8 @@
 
 import sys
 
-from pytgl.telegram import Telegram
 from pytgl import ffi, tgl
+from pytgl.telegram import Telegram
 from pytgl.callbacks import generate_tgl_update
 
 
@@ -25,35 +25,39 @@ def _msg_cb(tls, msg):
         print("New message: ", ffi.string(msg.message).decode())
 
 
-if len(sys.argv) != 2:
-    print("Usage: %s telegram_rsa_key_path" % sys.argv[0])
-    sys.exit()
+def main():
+    upd_cb = generate_tgl_update()
+    upd_cb.new_msg = _msg_cb
+    upd_cb.msg_receive = _msg_cb
 
 
-upd_cb = generate_tgl_update()
-upd_cb.new_msg = _msg_cb
-upd_cb.msg_receive = _msg_cb
+    tg = Telegram(rsa_key = sys.argv[1],
+                  update_callbacks = upd_cb)
+
+    #tg._state.verbosity = 6
+
+    #tg.reset_authorization()
+    tg.wait_until_authorization()
+    tg.check_authorization()
+    print("All DCs are authorized.")
+
+    tg.sign_in()
+    tg.check_sign_in()
+    print("Signed in.")
+
+    tg.store_auth()
+
+    tg.send_all_unsent()
+    print("All unsent msgs are sent.")
+
+    tg.get_difference()
+    print("Entering main loop...")
+    tg.loop()
 
 
-tg = Telegram(rsa_key = sys.argv[1],
-              update_callbacks = upd_cb)
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: %s telegram_rsa_key_path" % sys.argv[0])
+        sys.exit(2)
 
-#tg._state.verbosity = 6
-
-#tg.reset_authorization()
-tg.wait_until_authorization()
-tg.check_authorization()
-print("All DCs are authorized.")
-
-tg.sign_in()
-tg.check_sign_in()
-print("Signed in.")
-
-tg.store_auth()
-
-tg.send_all_unsent()
-print("All unsent msgs are sent.")
-
-tg.get_difference()
-print("Entering main loop...")
-tg.loop()
+    main()
